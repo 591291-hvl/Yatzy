c0d4a3f98ecb63e4004feff89086078d78f73a33
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-06 00:39:25 +0100

    man i suck at documenting code, added way to reset dice, set up player and yatzy class

---
+++ src/main/java/main/Yatzy.java
@@ -0,0 +1,21 @@
+package main;
+
+
+public class Yatzy {
+
+    DiceShaker shaker;
+
+    /**
+     * Initializes classes to use in game
+     * @param numberOfDices number of dices to play by
+     */
+    public Yatzy(int numberOfDices){
+        //create dice shaker, option to choose number of dices
+        this.shaker = new DiceShaker(numberOfDices);
+
+        //players
+
+        //board
+
+    }
+}
