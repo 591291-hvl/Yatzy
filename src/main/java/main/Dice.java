121774ed25e2090082f43a3269578e00c0ddab6d
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-05 23:16:59 +0100

    ability to lock dices, so they do not change value

--- src/main/java/main/Dice.java
+++ src/main/java/main/Dice.java
@@ -4,41 +4,55 @@ import java.util.Random;
 
 /**
  * @author Daniel K.Gunleiksrud
- *
  */
 public class Dice {
-	
-	private Random random = new Random();
-	
-	private int value;
-	private boolean locked;
-	
-	/**
-	 * value is -1 if not rolled, not used
-	 * dice has no initial value, have to be rolled
-	 */
-	public Dice() {
-		this.value = -1;
-		this.locked = false;
-	}
-	
-	/**
-	 * rolls a 6 sided dice
-	 * @return int
-	 */
-	public int roll() {
-		return random.nextInt(6)+1;
-	}
-	
-	/**
-	 * true if locked -> do not roll again
-	 * false if not locked -> change value
-	 * @return
-	 */
-	public boolean isLocked() {
-		return locked;
-	}
-	
-	
+
+    private Random random = new Random();
+
+    private int value;
+    private boolean locked;
+
+    /**
+     * value is -1 if not rolled, not used
+     * dice has no initial value, have to be rolled
+     */
+    public Dice() {
+        this.value = -1;
+        this.locked = false;
+    }
+
+    /**
+     * rolls a 6 sided dice
+     *
+     * @return int
+     */
+    public int roll() {
+        return random.nextInt(6) + 1;
+    }
+
+    /**
+     * true if locked -> do not roll again
+     * false if not locked -> change value
+     *
+     * @return
+     */
+    public boolean isLocked() {
+        return locked;
+    }
+
+    /**
+     * locks dice
+     */
+    public void lock() {
+        this.locked = true;
+    }
+
+    /**
+     * unlocks dice
+     */
+    public void unlock() {
+        this.locked = false;
+    }
+
 
 }
--- src/main/java/main/DiceShaker.java
+++ src/main/java/main/DiceShaker.java
@@ -4,61 +4,61 @@ import java.util.Arrays;
 
 /**
  * @author Daniel K.Gunleiksrud
- *
  */
 public class DiceShaker {
-	
-	Dice[] shaker;
-	int numberOfDices;
-	
-	/**
-	 * Container for multiple dices
-	 * Creates terning in kopp
-	 */
-	public DiceShaker(int numberOfDices) {
-		this.shaker = new Dice[numberOfDices];
-		this.numberOfDices = numberOfDices;
-		
-		for(int i = 0; i < numberOfDices; i = -~i) {
-			shaker[i] = new Dice();
-		}
-		
-	}
-	
-	/**
-	 * Gives new value to all dices expect locked dice
-	 */
-	public void shakeDice() {
-		for(int i = 0; i < numberOfDices; i = -~i) {
-			if(!shaker[i].isLocked()) {
-				shaker[i].roll();
-			}
-		}
-	}
-	
-	/**
-	 * returns diceshaker
-	 * @return Dice[]
-	 */
-	public Dice[] getDices() {
-		return shaker;
-	}
-	
-	/**
-	 * locks/unlocks dice
-	 * @param int array of dices to lock
-	 */
-	public void lockDice(int[] toLock) {
-		
-		String toLockString = Arrays.toString(toLock);
-		
-		for(int i = 0; i < numberOfDices; i = -~i) {
-			if(toLockString.contains(Integer.toString(i))) {
-//				shaker[i]
-			}
-		}
-	}
-	
-	
 
+    Dice[] shaker;
+    int numberOfDices;
+
+    /**
+     * Container for multiple dices
+     * Creates terning in kopp
+     */
+    public DiceShaker(int numberOfDices) {
+        this.shaker = new Dice[numberOfDices];
+        this.numberOfDices = numberOfDices;
+
+        for (int i = 0; i < numberOfDices; i = -~i) {
+            shaker[i] = new Dice();
+        }
+
+    }
+
+    /**
+     * Gives new value to all dices expect locked dice
+     */
+    public void shakeDice() {
+        for (int i = 0; i < numberOfDices; i = -~i) {
+            if (!shaker[i].isLocked()) {
+                shaker[i].roll();
+            }
+        }
+    }
+
+    /**
+     * returns diceshaker
+     *
+     * @return Dice[]
+     */
+    public Dice[] getDices() {
+        return shaker;
+    }
+
+    /**
+     * locks/unlocks dice
+     *
+     * @param toLock int array of dices to lock
+     */
+    public void lockDice(int[] toLock) {
+
+        String toLockString = Arrays.toString(toLock);
+
+        for (int i = 0; i < numberOfDices; i = -~i) {
+            if (toLockString.contains(Integer.toString(i))) {
+                shaker[i].lock();
+                continue;
+            }
+            shaker[i].unlock();
+        }
+    }
 }
