f2c51f1ebd941095701f61a75d66da2c920ccbc4
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-06 14:49:57 +0100

    started game logic

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -7,6 +12,13 @@ public class Main {
 		Yatzy yatzy = new Yatzy(5,4);
 
 		yatzy.display();
+
+		yatzy.playGame();
+
+
+
+
+
 
 
 
--- src/main/java/main/Yatzy.java
+++ src/main/java/main/Yatzy.java
@@ -1,6 +1,12 @@
 package main;
 
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Yatzy {
 
     DiceShaker shaker;
@@ -12,17 +18,20 @@ public class Yatzy {
 
     /**
      * Initializes classes to use in game
-     * @param numberOfDices number of dices to play by
+     *
+     * @param numberOfDices   number of dices to play by
      * @param numberOfPlayers number of players:)
      */
-    public Yatzy(int numberOfDices, int numberOfPlayers){
+    public Yatzy(int numberOfDices, int numberOfPlayers) {
         //create dice shaker, option to choose number of dices
         this.shaker = new DiceShaker(numberOfDices);
         this.numberOfDices = numberOfDices;
         this.numberOfPlayers = numberOfPlayers;
 
         //players
-        this.players = new Player(numberOfPlayers);
+        //this.players = new Player(numberOfPlayers);
+        String[] playerNames = {"Daniel", "Harald", "Per", "Petter"};
+        this.players = new Player(playerNames);
 
         //board
         this.board = new Board(numberOfPlayers);
@@ -33,15 +42,67 @@ public class Yatzy {
     /**
      * prints how the board looks like in console
      */
-    public void display(){
-        String stringBoard = String.format("%-20s", "Players");;
+    public void display() {
+        String stringBoard = String.format("%-20s", "Players");
+        ;
         String[] playerId = players.getPlayers();
-        for (int i = 0; i < numberOfPlayers; i = -~i){
+        for (int i = 0; i < numberOfPlayers; i = -~i) {
             stringBoard += String.format("%-10s", playerId[i]);
         }
         stringBoard += "\n" + board.toString();
 
         System.out.println(stringBoard);
+    }
+
+    /**
+     * Plays round for one player
+     *
+     * @param id player id
+     */
+    public void playPlayerRound(int id) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Player " + players.getPlayer(id) + "'s turn");
+
+        //roll happens 3 times, ability to keep 2 times
+
+
+        for (int i = 0; i < 2; i = -~i) {
+            //roll and display
+            shaker.shakeDice();
+            shaker.display();
+
+            //option to keep
+            //create int array from sc input
+            System.out.println("Write position of dice to keep 0-5, seperated by space");
+            int[] diceToLock = Stream.of(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).map(x -> ~-x).toArray();
+            //lock dice
+            shaker.lockDice(diceToLock);
+        }
+        System.out.println("Results: ");
+        shaker.shakeDice();
+        shaker.display();
+
+        //store value
+
+        //dice back in shaker
+        shaker.reset();
+    }
+
+    /**
+     * Play one round, each player gets to play
+     */
+    public void playOneRound() {
+
+        for (int i = 0; i < numberOfPlayers; i = -~i) {
+            playPlayerRound(i);
+        }
+    }
+
+    public void playGame() {
+        //rounds to play 6 + 9
+        for (int i = 0; i < 6; i = -~i) {
+            playOneRound();
+        }
     }
 
 
