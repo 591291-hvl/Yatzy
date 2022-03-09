6cd314c72517d5aa5a0c564a3d75d46e98927c84
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-09 13:04:39 +0100

    more bitwise, and board toString test

--- src/test/java/tests/BoardTest.java
+++ src/test/java/tests/BoardTest.java
@@ -24,4 +24,55 @@ public class BoardTest {
 
     }
 
+    @Test
+    public void printTest() {
+        Board board = new Board(4);
+
+        //idk if this is ok, but code coverage goes up ¯\_(ツ)_/¯
+        assertEquals(board.toString()
+                , "One's               -         -         -         -         \n" +
+                "Two's               -         -         -         -         \n" +
+                "Three's             -         -         -         -         \n" +
+                "Four's              -         -         -         -         \n" +
+                "Five's              -         -         -         -         \n" +
+                "Six's               -         -         -         -         \n" +
+                "Sum                 -         -         -         -         \n" +
+                "Bonus               -         -         -         -         \n" +
+                "One pair            -         -         -         -         \n" +
+                "Two pair's          -         -         -         -         \n" +
+                "Three of a kind     -         -         -         -         \n" +
+                "Four of a kind      -         -         -         -         \n" +
+                "Small straight      -         -         -         -         \n" +
+                "Big straight        -         -         -         -         \n" +
+                "Full house          -         -         -         -         \n" +
+                "Chance              -         -         -         -         \n" +
+                "YATZY               -         -         -         -         \n" +
+                "Total Sum           -         -         -         -         \n");
+
+
+        board.setValue(0,0,1);
+        board.setValue(1,0,~0);
+
+        assertEquals(board.toString()
+                , "One's               1         0         -         -         \n" +
+                        "Two's               -         -         -         -         \n" +
+                        "Three's             -         -         -         -         \n" +
+                        "Four's              -         -         -         -         \n" +
+                        "Five's              -         -         -         -         \n" +
+                        "Six's               -         -         -         -         \n" +
+                        "Sum                 -         -         -         -         \n" +
+                        "Bonus               -         -         -         -         \n" +
+                        "One pair            -         -         -         -         \n" +
+                        "Two pair's          -         -         -         -         \n" +
+                        "Three of a kind     -         -         -         -         \n" +
+                        "Four of a kind      -         -         -         -         \n" +
+                        "Small straight      -         -         -         -         \n" +
+                        "Big straight        -         -         -         -         \n" +
+                        "Full house          -         -         -         -         \n" +
+                        "Chance              -         -         -         -         \n" +
+                        "YATZY               -         -         -         -         \n" +
+                        "Total Sum           -         -         -         -         \n");
+
+    }
+
 }
