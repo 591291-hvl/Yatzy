d41c6cedf9afd3844cdac7d9dfac7941606fd615
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-29 10:54:37 +0200

    shaking and crying

---
+++ src/main/java/database/PersonEntity.java
@@ -0,0 +1,51 @@
+package database;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Person", schema = "public", catalog = "h591291")
+public class PersonEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false, length = -1)
+    private String id;
+    @Basic
+    @Column(name = "name", nullable = true, length = -1)
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PersonEntity that = (PersonEntity) o;
+
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+}
