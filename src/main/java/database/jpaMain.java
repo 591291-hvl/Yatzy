2ac41544a23e9eec847b9b3a09ec20d91e7ac1d4
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-30 09:59:21 +0200

    help, connection to database
    i have no clue what i am doing, but this is the closest i have gotten

--- src/main/java/database/jpaMain.java
+++ src/main/java/database/jpaMain.java
@@ -1,32 +1,43 @@
 package database;
 
 
-import javax.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
 import java.util.List;
 
 public class jpaMain {
 
     public static void main(String[] args){
+        try {
+            System.out.println(Class.forName("org.postgresql.Driver"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("personUnit");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("PersonUnit");
         EntityManager em = emf.createEntityManager();
-
-        String name = "Daniel";
+        //String name = "Daniel";
 
         try{
-            TypedQuery<Person> query = em.createQuery("SELECT p FROM Person p WHERE p.name = '" + name + "'", Person.class);
-            List<Person> persons = query.getResultList();
 
+            TypedQuery<PersonEntity> query = em.createQuery("SELECT p FROM PersonEntity p", PersonEntity.class);
+            List<PersonEntity> persons = query.getResultList();
 
-
-            for(Person p: persons){
+            for(PersonEntity p: persons){
                 System.out.println(p + "<---");
-
             }
 
+        } catch (Exception e){
+            System.out.println("===============================");
+            System.out.println(e);
+            System.out.println("===============================");
         } finally {
-            emf.close();
             em.close();
+            emf.close();
+
         }
 
     }
