ff36707e2b927883dec1930f247b3959ff3a67ac
Daniel K.Gunleiksrud <591291@stud.hvl.no>
2022-03-30 11:38:23 +0200

    refactor + DAO

---
+++ src/main/java/jpaMain.java
@@ -0,0 +1,30 @@
+import dao.PersonDAO;
+import entity.PersonEntity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class jpaMain {
+
+    public static void main(String[] args){
+        try {
+            System.out.println(Class.forName("org.postgresql.Driver"));
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        PersonDAO pDAO = new PersonDAO();
+
+        List<PersonEntity> persons = pDAO.getAllPersons();
+
+        for(PersonEntity p : persons){
+            System.out.println(p + "<-----");
+        }
+
+
+
+    }
+}
